692f0f31dbb0c4b15b9b7cf5a15c56e76bf138b4
GIO <GIO@DESKTOP-BQDO65U>
2024-05-12 23:18:16 -0500

    Agregar archivos de proyecto.

---
+++ Model/Productos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semana6GPaucar.Model
+{
+    public class Productos
+    {
+
+        public int id { get; set; }
+        public string producto { get; set; }
+        public string categoria { get; set;}
+        public double precio { get; set;}
+    }
+}
